2704409d2adbc99ff20be749d5130886547c8568
Paul Valdez <pvaldez11295@gmail.com>
2019-01-30 16:13:17 -0500

    added styles, pages, and plugins that allow for an asynchronous UX

---
+++ src/pages/tags.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const TagsPage = () => (
+  <Layout>
+    <SEO title="Tags" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>tagspage</h1>
+  </Layout>
+)
+
+export default TagsPage;
---
+++ src/pages/team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Team" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>our team</h1>
+  </Layout>
+)
+
+export default TeamPage
