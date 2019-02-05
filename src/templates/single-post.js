d14ee9680a77906691b78331ff778b4120cd6690
Paul Valdez <pvaldez11295@gmail.com>
2019-02-05 13:32:23 -0500

    implemented Disqus plugin and added social media share buttons]

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -6,10 +6,21 @@ import { Badge, Card, CardBody, CardSubtitle } from 'reactstrap';
 import Img from 'gatsby-image';
 import { slugify } from '../util/utilityFunctions';
 import authors from '../util/authors';
+import { DiscussionEmbed } from 'disqus-react';
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
   const post = data.markdownRemark.frontmatter
   const author = authors.find(x => x.name === post.author)
+
+  const baseUrl = 'https://valdezdev.github.io/'
+
+  const disqusShortname = 'https-valdezdev-github-io'
+  const disqusConfig = {
+    identifier: data.markdownRemark.id,
+    title: post.title,
+    url: baseUrl + pageContext.slug
+  }
+
   return (
     <Layout
       pageTitle={post.title}
@@ -39,6 +50,50 @@ const SinglePost = ({ data }) => {
           </ul>
         </CardBody>
       </Card>
+      <h3 className="text-center">
+              Share this post
+      </h3>
+      <div className="text-center social-share-links">
+        <ul>
+          <li>
+            <a
+              href={ 'https://www.facebook.com/sharer/sharer.php?u=' + baseUrl + pageContext.slug }
+              className="facebook"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-facebook-f fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://twitter.com/share?url=' + baseUrl + pageContext.slug + '&text=' + post.title + '&via' + 'tiwtterHandle'}
+              className="twitter"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-twitter fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://plus.google.com/share?url=' + baseUrl + pageContext.slug }
+              className="google"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-google fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://www.linkedin.com/shareArticle?url=' + baseUrl + pageContext.slug }
+              className="linkedin"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-linkedin fa-2x"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
     </Layout>
   )
 }
